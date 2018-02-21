4c9e45ee8988d82f149953e716c0f0c65a4eb113
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-20 18:12:03 -0800

    created texteditor component; need to fix syntax errors

---
+++ client/pages/TextEditor.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Editor, EditorState, RichUtils } from 'draft-js';
+
+class TextEditor extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editorState = EditorState.createEmpty(),
+    }
+    this.onChange = (editorState) => this.setState({ editorState })
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
+  }
+
+  render() {
+    return (
+      <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+      <Editor editorState={this.state.editorState}
+        onChange={this.onChange}
+        handleKeyCommand={this.handleKeyCommand}
+      />
+    )
+  }
+}
+
+export default TextEditor;
+
