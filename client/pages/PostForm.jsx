baf659f7ece5b434e9be0e413386164c0792be74
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-21 11:19:12 -0800

    Created an id for submission form

---
+++ client/pages/PostForm.jsx
@@ -0,0 +1,51 @@
+const React = require('react');
+const ReactMarkdown = require('react-markdown');
+
+// const input = 'hello from **textbox** heheheheh'
+
+// const TextBox = () => <ReactMarkdown source={input} />
+
+class PostForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      titleText: '',
+      bodyText: ''
+    }
+  }
+
+  onTitleTextChangeHandler = (e) => {
+    this.setState({ titleText: e.target.value }, () => {
+      console.log(this.state.titleText)
+    })
+  }
+
+  onBodyTextChangeHandler = (e) => {
+    this.setState({ bodyText: e.target.value }, () => {
+      console.log(this.state.bodyText)
+    })
+  }
+
+
+  render() {
+    return (
+      <div id="postSubmission">
+        Title: <br />
+        <textarea rows="1" cols="50" value={this.state.titleText} onChange={this.onTitleTextChangeHandler}>
+        </textarea> <br />
+
+        Text <br />
+        <textarea row="5" cols="50" value={this.state.bodyText} onChange={this.onBodyTextChangeHandler}>
+        </textarea>
+        {/* Renders HTML text */}
+        <ReactMarkdown source={this.state.bodyText} />
+
+        <button>Post!</button>
+      </div>
+    )
+  }
+}
+
+
+export default PostForm;
+
