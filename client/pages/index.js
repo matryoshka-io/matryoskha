e0637247aa37d836db85e2fba4710dc51465f8dc
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-21 18:59:47 -0800

    updated with current homepage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,51 +1,29 @@
-import PostForm from './PostForm.jsx'
-import axios from 'axios'
-// const Index = (props) => (
-// <div>
-//   <h1>Matryoshka Stack +1</h1>
-//   <h2>{props.message}</h2>
-//   <style jsx>
-//     {`
-//     h1 { font-size: 36px; color: #333;}
-//     h2 { margin-left: 16px; }
-//   `}
-//   </style>
-// </div>
-// );
-
-// Index.getInitialProps = async function () {
-//   // initial data requests happen in here
-//   // they are passed to props above automatically
-//   return {
-//     message: "There are many dolls, but this one is ours"
-//   }
-// };
-
-class Index extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: "There are many dolls, but this one is ours"
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Matryoshka Stack +1</h1>
-        <h2>{this.state.message}</h2>
-        <style jsx>
-          {`
-        h1 { font-size: 36px; color: #333;}
-        h2 { margin-left: 16px; }
+const Homepage = props => (
+  <div>
+    <h1>{props.url.asPath}</h1>
+    <h2>You are everywhere and nowhere all at once</h2>
+    <style jsx>
+      {`
+        h1 {
+          font-size: 36px;
+          color: #333;
+        }
+        h2 {
+          margin-left: 16px;
+        }
       `}
-        </style>
+    </style>
+  </div>
+);
 
-        <PostForm />
-        {/* ^ render that on profile post pages! */}
-      </div>
-    )
+Homepage.getInitialProps = async function () {
+  // initial data requests happen in here
+  // they are passed to props above automatically
+  return {
+    navigation: {},
+    user: {},
+    posts: {},
   }
-}
+};
 
-export default Index;
+export default Homepage;
