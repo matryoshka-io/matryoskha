b8a7b0eaeed48fed7220e7544bb8bef24128fa47
James Yen <jameshyen@gmail.com>
2018-02-22 11:52:06 -0800

    Modularity

---
+++ server/controllers/subreddit.js
@@ -0,0 +1,25 @@
+const db = require('../database');
+const models = require('../models');
+
+module.exports = {
+  POST: function (req, res) {
+    // For testing.
+    req.session = {
+      user: '5a8e0e077f911450d4600d96', // i.e. admin, see dummy data
+    };
+    // Use shortcut later. Omitting the date because the default is "now."
+    const newSubredditSkeleton = {
+      creator: req.session.user, // I'm assuming that we're using npm – express-session, in which case the User's ID will be attached to the session
+      description: req.body.description,
+      title: req.body.title,
+    };
+
+    const newSubreddit = new models.Subreddit(newSubredditSkeleton);
+    newSubreddit.save().then(function (subreddit) {
+      res.status(201).end(JSON.stringify(subreddit)); // Does this violate REST? Then again, the TA...
+    });
+  },
+  GET: function (req, res) {
+    res.status(404).end('Unimplemented!');
+  },
+};
