867a92817ce1745f36a164bb481753c98f315e47
Truelav <serghei.madan@gmail.com>
2018-02-22 15:26:05 -0800

    added login form and a few components for subreddit

---
+++ client/components/rating.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default () => (
+    <div classname="vote">
+<p><a href="#">&#x25B2;</a></p>
+<p>10.5k</p>
+<p><a href="#">&#x25BC;</a></p>
+   <style jsx>{`
+    
+   `}</style>
+    </div>
+)
