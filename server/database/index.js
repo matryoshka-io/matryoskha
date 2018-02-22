b821bf75d513b8a96354262b868f23bccf02cc63
James Yen <jameshyen@gmail.com>
2018-02-21 16:09:45 -0800

    Gonna try to make this "modular"

--- server/database/index.js
+++ server/database/index.js
@@ -3,26 +3,24 @@ mongoose.connect('mongodb://localhost:27017/matryoksha');
 
 const db = mongoose.connection;
 
+const Link = require('../models/Link');
+const Post = require('../models/Post');
+const Subreddit = require('../models/Subreddit');
+const Subscription = require('../models/Subscription');
+const User = require('../models/User');
+const Vote = require('../models/Vote');
+
+const data = JSON.parse(require('./data.json'));
+
 db.on('error', function (error) {
   console.log('Error connnecting to database: ' + error);
 });
 
 db.once('open', function () {
   console.log('Mongoose successfully connected to MongoDB');
+  console.log('Inserting dummy data');
 
-  const testSchema = mongoose.Schema({
-    date: {
-      type: Date,
-      default: Date.now,
-    }
-  });
-
-  const DateModel = mongoose.model('Date', testSchema);
-  const newDate = new DateModel();
-
-  newDate.save().then(function (date) {
-    console.log(date);
-  });
+  // for (const link in data[])
 });
 
 module.exports = db;
