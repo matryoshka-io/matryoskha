f93ae25caad3509853918d2c6cc473ba8097841f
James Yen <jameshyen@gmail.com>
2018-02-21 16:19:12 -0800

    No comments in JSON

--- server/database/index.js
+++ server/database/index.js
@@ -10,7 +10,17 @@ const Subscription = require('../models/Subscription');
 const User = require('../models/User');
 const Vote = require('../models/Vote');
 
-const data = JSON.parse(require('./data.json'));
+const models = {
+  Link,
+  Post,
+  Subreddit,
+  Subscription,
+  User,
+  Vote,
+};
+
+let data = require('./data.json');
+data = JSON.parse(data);
 
 db.on('error', function (error) {
   console.log('Error connnecting to database: ' + error);
@@ -20,7 +30,17 @@ db.once('open', function () {
   console.log('Mongoose successfully connected to MongoDB');
   console.log('Inserting dummy data');
 
-  // for (const link in data[])
+  const modelNames = Object.keys(models);
+  const promises = [];
+  for (const modelName of modelNames) {
+    for (const fakeData of data[modelName]) {
+      const newData = new models[modelName](fakeData);
+      promises.push(newData.save());
+    }
+  }
+  Promise.all(promises).then(function () {
+    console.log('Insertion of dummy data complete!');
+  });
 });
 
 module.exports = db;
