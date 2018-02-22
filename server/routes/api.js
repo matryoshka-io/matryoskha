1b6a94b32dde179dd17abd05e220f698c6413f0c
James Yen <jameshyen@gmail.com>
2018-02-22 12:32:12 -0800

    Can now make posts inside Subreddits

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,8 @@ const controllers = require('../controllers');
 
 router.get('/', controllers.home.GET);
 
-router.post('/sub', controllers.subreddit.POST);
+router.post('/sub', controllers.subreddit.POST.subreddit);
 router.get('/sub/:subName', controllers.subreddit.GET); // Figure that out on the client. Id (the huge ObjectId thing, *shudders*) or Subreddit name/title.
+router.post('/sub/:subName', controllers.subreddit.POST.post);
 
 module.exports = router;
