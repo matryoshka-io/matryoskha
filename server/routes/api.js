888a2fbd47354ec5e26cb9b203194abb3752f72c
James Yen <jameshyen@gmail.com>
2018-02-22 12:01:23 -0800

    Definitely violating DRY, but, MVP

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,6 @@ const controllers = require('../controllers');
 router.get('/', controllers.home.GET);
 
 router.post('/sub', controllers.subreddit.POST);
-router.get('/sub/:subId', controllers.subreddit.GET);
+router.get('/sub/:subName', controllers.subreddit.GET); // Figure that out on the client. Id (the huge ObjectId thing, *shudders*) or Subreddit name/title.
 
 module.exports = router;
