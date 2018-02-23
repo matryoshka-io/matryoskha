3208d854021bb49f92f3050df87943bf58aa2454
James Yen <jameshyen@gmail.com>
2018-02-23 10:34:28 -0800

    Annoyingly can't pass data between promises
    http://2ality.com/2017/08/promise-callback-data-flow.html

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -5,10 +5,23 @@ const utils = require('./utils');
 
 module.exports = {
   GET(req, res) {
-
+    models.Post.findOne({ type: 'Comment', _id: req.params.commentId })
+      .then((comment) => {
+        res.status(200).end(JSON.stringify(comment));
+      }).catch((err) => {
+        res.status(404).end(`No such comment found for id: ${req.params.commendId}`);
+      });
   },
+  // Admins should be able to edit comments (i.e. owners/creators of subreddits).
   PUT(req, res) {
-
+    req.session = {
+      username: 'admin',
+    };
+    models.User.findOne(req.session)
+      .then((user) => {
+        return models.Post.findOne({ type: 'Comment', _id: req.params.commentId });
+      })
+      .then(())
   },
   DELETE(req, res) {
 
