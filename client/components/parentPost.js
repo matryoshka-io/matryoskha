7b31c883367b7918412e780e96c39b4f72da7b5d
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-23 12:26:12 -0800

    Rendering with markdown

---
+++ client/components/parentPost.js
@@ -0,0 +1,84 @@
+// import Link from 'next/link'
+// import SubRedditBar from './subbredditBar'
+// import Rating from './rating'
+
+// export default ({_id, subreddit, title, body, date, comments}) => (
+
+//   <div style={{'border': 'solid', 'margin' : '10px', 'padding': '10px'}}>
+//      <Link href={subreddit}><a>{subreddit.title}</a></Link>
+//      <span style={{'float': 'right'}}>created at: {subreddit.date}</span>
+//      <div>desc: {subreddit.description}</div>
+//      <Rating />
+//      <div>
+//        <SubRedditBar className="subbredditbar"/>
+//      </div>
+//      <style jsx>{`
+//        .subbredditbar {
+//          float: left
+//        }
+//      `}
+//      </style>
+//    </div>
+
+// )
+
+import Link from 'next/link'
+import SubRedditBar from './subbredditBar'
+import Rating from './rating'
+import React from 'react'
+import axios from 'axios'
+import ReactMarkdown from 'react-markdown'
+
+
+class ParentPost extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      subreddit: '',
+      newBodyText: ''
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get('/api')
+      .then(res => {
+        return res.data.forEach(post => {
+          let subredditID = post.subreddit._id;
+          let bodyTextForEachPost = post.body
+          this.setState({
+            subredditId: subredditID,
+            newBodyText: bodyTextForEachPost
+          })
+        })
+        // return axios.get(`/api/sub/${this.state.subredditId}/post`)
+      })
+      .then(res => {
+        console.log('res', res)
+      })
+
+    // axios.get('/api/sub/')
+    //   .then(res => {
+    //     console.log('res', res.data.body)
+    //   })
+  }
+
+  // getBodyTextFromDB = () => {
+  //   axios.get('/')
+  //     .then(res => {
+  //       console.log('res on post', res)
+  //     })
+  // }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <ReactMarkdown source={this.state.newBodyText} />
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default ParentPost;
