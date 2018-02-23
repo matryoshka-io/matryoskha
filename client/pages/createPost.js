6252f4ece2912e512c3aaf149f69193055284c6c
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-23 12:03:44 -0800

    changed file name, rendering form onto view

---
+++ client/pages/createPost.js
@@ -0,0 +1,24 @@
+import PostForm from '../components/PostForm'
+
+const Post = (props) => (
+  <div>
+    <h1>{props.url.asPath}</h1>
+    <PostForm />
+  </div>
+)
+
+Post.getInitialProps = async function () {
+
+  return {
+    navigation: {},       //  every page need this?
+    user: {
+      session: {},        //  state of the user session
+      profile: {},        //  basic data for the user panel
+      subscriptions: [],  //  or isSubscribed for the parent subreddit
+    },
+    post: {},             //  main post content
+    comments: {}          //  initial comments load
+  };
+};
+
+export default Post;
