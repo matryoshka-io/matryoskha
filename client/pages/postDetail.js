02ae3f45eab48f1652a138c5faea4da393f93813
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-23 15:09:20 -0800

    renaming file to be more specific, rendering body with HTML

---
+++ client/pages/postDetail.js
@@ -0,0 +1,24 @@
+import ParentPost from '../components/parentPost'
+
+const PostPage = (props) => (
+  <div>
+    <h1>{props.url.asPath}</h1>
+    <ParentPost />
+  </div>
+)
+
+PostPage.getInitialProps = async function () {
+
+  return {
+    navigation: {},       //  every page need this?
+    user: {
+      session: {},        //  state of the user session
+      profile: {},        //  basic data for the user panel
+      subscriptions: [],  //  or isSubscribed for the parent subreddit
+    },
+    post: {},             //  main post content
+    comments: {}          //  initial comments load
+  };
+};
+
+export default PostPage;
