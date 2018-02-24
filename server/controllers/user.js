5c6206e3757a028e2fa0f4b95285e88151f96898
James Yen <jameshyen@gmail.com>
2018-02-24 09:27:02 -0800

    Vote controller

---
+++ server/controllers/user.js
@@ -0,0 +1,16 @@
+const db = require('../database');
+const models = require('../models');
+
+const utils = require('./utils');
+
+// Right now without bcrypt.
+module.exports = {
+  user: {
+    POST(req, res) {
+
+    },
+    PUT(req, res) {
+
+    },
+  },
+};
