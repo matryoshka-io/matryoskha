196796b6f5c833cc6f0cfd8135ee95e80eacc702
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-23 16:27:52 -0800

    move change handler to different file

--- client/pages/postDetail.js
+++ client/pages/postDetail.js
@@ -1,33 +1,17 @@
 import ParentPost from '../components/parentPost'
 import CommentForm from '../components/commentForm'
 
-// const PostPage = (props) => (
-//   <div>
-//     <h1>{props.url.asPath}</h1>
-//     <ParentPost />
-
-//     Post a comment:
-//     <CommentForm />
-//   </div>
-// )
-
-// PostPage.getInitialProps = async function () {
-
-//   return {
-//     navigation: {},       //  every page need this?
-//     user: {
-//       session: {},        //  state of the user session
-//       profile: {},        //  basic data for the user panel
-//       subscriptions: [],  //  or isSubscribed for the parent subreddit
-//     },
-//     post: {},             //  main post content
-//     comments: {}          //  initial comments load
-//   };
-// };
-
 class PostPage extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      commentText: ''
+    }
+
+  }
+
+  componentDidMount() {
+    //all comments related to thread post
   }
 
   getInitialProps = async function () {
@@ -44,14 +28,16 @@ class PostPage extends React.Component {
     };
   };
 
+
+
   render() {
     return (
       <div>
-        <h1>{props.url.asPath}</h1>
+        <h1>{this.props.url.asPath}</h1>
         <ParentPost />
 
         Post a comment:
-        <CommentForm />
+        <CommentForm commentText={this.state.commentText} commentOnChangeHandler={this.commentOnChangeHandler} />
       </div>
     )
   }
