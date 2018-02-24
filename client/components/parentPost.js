4cdcbede2eb4a36716f21ae8fd71a3b02b6e5862
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-24 09:54:39 -0800

    attempting to render comments under parent post

--- client/components/parentPost.js
+++ client/components/parentPost.js
@@ -5,15 +5,18 @@ import React from 'react'
 import axios from 'axios'
 import ReactMarkdown from 'react-markdown'
 import Postdetail from './postDetails'
+import CommentForm from './commentForm'
 
 
 class ParentPost extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      subreddit: '',
+      subredditId: '',
+      postId: '',
       title: '',
       newBodyText: '',
+      comments: []
     }
   }
 
@@ -21,20 +24,40 @@ class ParentPost extends React.Component {
     axios.get('/api')
       .then(res => {
         return res.data.forEach(post => {
+          console.log('comment collection', post.comments)
+          console.log('type', Array.isArray(post.comments))
           let subredditID = post.subreddit._id;
           let bodyTextForEachPost = post.body;
           this.setState({
             subredditId: subredditID,
             title: post.title,
-            newBodyText: bodyTextForEachPost
-          })
+            newBodyText: bodyTextForEachPost,
+            postId: post._id,
+            comments: post.comments,
+          }, () => console.log('???', this.state.comments))
         })
-        // return axios.get(`/api/sub/${this.state.subredditId}/post`)
+          .then(res => {
+            axios.get(`/api/sub/${this.state.subredditId}/post/${this.state.postId}`)
+          })
       })
       .then(res => {
-        console.log('SUCCESSFUL GET')
+        console.log('SUCCESSFUL MOUNT')
       })
+    // axios.get('post/5a8e0e2b7f911450d4600d99')
   }
+
+  // componentDidMount() {
+  //   axios.get('/api')
+  //     .then(res => {
+  //       res.data.forEach(data => {
+  //         if (res.data.title === /*subreddit title*/) {
+
+  //         }
+  //       })
+  //     })
+  //   // axios.get(`/post/${postID}`)
+  // }
+
 
   render() {
     return (
@@ -44,6 +67,10 @@ class ParentPost extends React.Component {
           {this.state.title} <br /><br />
           Body: <ReactMarkdown source={this.state.newBodyText} />
         </div>
+
+        <CommentForm title={this.state.title} subredditId={this.state.subredditId} />
+
+        {this.state.comments.toString()}
       </div>
 
     )
