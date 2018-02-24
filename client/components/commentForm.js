4b4d9d10569e017800572520edc02d126222c95b
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-23 16:28:21 -0800

    change handler function now here

--- client/components/commentForm.js
+++ client/components/commentForm.js
@@ -1,22 +1,63 @@
-export default () => (
-    <div>
-      <textarea />
-      <button>add comment</button>
-      <style jsx>{`
-        textarea {
-          width: 400px;
-          height: 100px;
-          display: block;
-          margin-bottom: 10px;
-        }
-        button {
-          padding: 3px 4px;
-        }
-        @media (max-width: 750px) {
+// export default () => (
+//   <div>
+//     <textarea />
+//     <button>add comment</button>
+//     <style jsx>{`
+//         textarea {
+//           width: 400px;
+//           height: 100px;
+//           display: block;
+//           margin-bottom: 10px;
+//         }
+//         button {
+//           padding: 3px 4px;
+//         }
+//         @media (max-width: 750px) {
+//           textarea {
+//             width: 100%;
+//           }
+//         }
+//       `}</style>
+//   </div>
+// )
+
+class CommentForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      commentText: ''
+    }
+  }
+
+  commentOnChangeHandler(e) {
+    this.setState({ commentText: e.target.value }, () => {
+      console.log('hello', this.state.commentText)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <textarea onChange={this.commentOnChangeHandler} />
+        <button>add comment</button>
+        <style jsx>{`
           textarea {
-            width: 100%;
+            width: 400px;
+            height: 100px;
+            display: block;
+            margin-bottom: 10px;
           }
-        }
-      `}</style>
-    </div>
-  )
+          button {
+            padding: 3px 4px;
+          }
+          @media (max-width: 750px) {
+            textarea {
+              width: 100%;
+            }
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+export default CommentForm;
