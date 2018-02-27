0a152d9aa76677b06f0a112518b97823b7062c0e
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-26 23:01:31 -0800

    fix syntax errors

--- client/components/ParentPost.js
+++ client/components/ParentPost.js
@@ -9,88 +9,52 @@ import CommentForm from './CommentForm';
 
 
 class ParentPost extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    subredditId: '',
-    postId: '',
-    postTitle: '',
-    postBodyText: '',
-    commentBody: '',
-    comments: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      subredditId: '',
+      postId: '',
+      postTitle: '',
+      postBodyText: '',
+      commentBody: '',
+      comments: []
+    }
   }
-}
 
-componentDidMount() {
-  // axios.get('/api')
-  //   .then(res => {
-  //     console.log('RESSS', res)
-  //   })
-  axios.get('/api/post/5a8e0e2b7f911450d4600d99')
-    .then(res => {
-      console.log('res', res)
-      this.setState({
-        postTitle: res.data.title,
-        postBodyText: res.data.body,
-        postId: res.data._id,
-        comments: res.data.comments
+  componentDidMount() {
+    // axios.get('/api')
+    //   .then(res => {
+    //     console.log('RESSS', res)
+    //   })
+    axios.get('/api/post/5a8e0e2b7f911450d4600d99')
+      .then(res => {
+        console.log('res', res)
+        this.setState({
+          postTitle: res.data.title,
+          postBodyText: res.data.body,
+          postId: res.data._id,
+          comments: res.data.comments
+        })
       })
-    })
-    // .then(res => {
-    //   console.log('res', res)
-    //   res.data.forEach(post => {
-    //     this.setState({
-    //       postTitle: post.title,
-    //       postBodyText: post.body,
-    //       postId: post._id
-    //     })
-    //   })
-    // })
-    // .then(res => {
-    //   console.log(res.data)
-    //   res.data.forEach(post => {
-    //     this.setState({
-    //       title: post.title,
-    //       postBodyText: post.body,
-    //       postId: post._id
-    //     })
-    //     if (post.comments.length === 0) {
-    //       this.setState({ comments: [] })
-    //     } else {
-    //       post.comments.forEach(comment => {
-    //         console.log('commentBody', this.state.commentBody)
-    //         this.setState({
-    //           comments: post.comments,
-    //           commentBody: comment.body
-    //         }, () => { console.log(this.state.commentBody) })
-    //       })
-    //     }
-    //   })
-    // })
-    // .then(res => {
-    //   axios.get(`/api/post/:${this.state.postId}`)
-    // })
-    // .then(res => {
-    //   console.log('RESPONSEEEEE', res)
-    // })
-  }
 
-  render() {
-    return (
-      <div>
+
+    render() {
+      return (
         <div>
-          Title of post: <br />
-          {this.state.postTitle} <br /><br />
-          Body: <ReactMarkdown source={this.state.postBodyText} />
+          <div>
+            Title of post: <br />
+            {this.state.postTitle} <br /><br />
+            Body: <ReactMarkdown source={this.state.postBodyText} />
+          </div>
+
+          <CommentForm title={this.state.title} subredditId={this.state.subredditId} />
+
+          <CommentList />
+          {/* {this.state.commentBody} */}
         </div>
 
-        <CommentForm title={this.state.title} subredditId={this.state.subredditId} />
-
-
-        {/* {this.state.commentBody} */}
-      </div>
-
-    );
+      );
+    }
   }
 }
 
