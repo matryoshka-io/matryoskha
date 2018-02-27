4b03170d22eab50328469554434b9ca36336ecce
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-27 08:25:21 -0800

    trying recursive comments

--- client/components/CommentListEntry.js
+++ client/components/CommentListEntry.js
@@ -1,10 +1,25 @@
-const CommentListEntry = (props) => {
-  return (
-    <div>
-      Comments:
-      {props.comment.body}
-    </div>
-  )
+// const CommentListEntry = (props) => {
+//   return (
+//     <div>
+//       Comments:
+//       {props.comment.body}
+//     </div>
+//   )
+// }
+import CommentList from './CommentList';
+
+class CommentListEntry extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      // console.log('hullo', this.props.comment.comments)
+      < div >
+        < CommentList commentsArr={this.props.comment.comments} />
+      </div >
+    )
+  }
 }
 
 export default CommentListEntry
