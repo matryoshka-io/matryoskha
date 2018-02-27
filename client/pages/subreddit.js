d70fa21c504291399fe621a2b575e0d7b416904d
James Yen <jameshyen@gmail.com>
2018-02-27 15:07:10 -0800

    Proof of concept

--- client/pages/subreddit.js
+++ client/pages/subreddit.js
@@ -1,7 +1,14 @@
+import auth from '../utils/auth';
+import data from '../utils/data';
+import sessions from '../utils/sessions';
+
+import 'isomorphic-fetch';
+
 const Subreddit = (props) => (
   <div>
     <h1>{props.url.asPath}</h1>
     <h2>{props.message}</h2>
+    <p>{props.posts[0].title}</p>
     <style jsx>
     {`
       h1 { font-size: 36px; color: #333;}
@@ -11,11 +18,19 @@ const Subreddit = (props) => (
   </div>
 );
 
-Subreddit.getInitialProps = async function () {
+Subreddit.getInitialProps = async function (context) {
   // initial data requests happen in here
   // they are passed to props above automatically
+  const session = await auth.initializeSession(context);
+  // const posts = await data.getPosts(session);
+  console.log(`http://localhost:3000/api/sub/${context.query.subreddit}`);
+  const subreddit = await fetch(`http://localhost:3000/api/sub/${context.query.subreddit}`);
+  const json = await subreddit.json();
+  console.log(json);
+
   return {
-    message: "There are many dolls, but this one is ours"
+    posts: json,
+    message: "There are many dolls, but this one is ours",
   };
 };
 
