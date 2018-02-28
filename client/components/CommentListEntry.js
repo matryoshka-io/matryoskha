896e423affb7f4e2447815eb7c007ee7fc689767
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-27 18:21:07 -0800

    adding boxes around comments

--- client/components/CommentListEntry.js
+++ client/components/CommentListEntry.js
@@ -7,16 +7,53 @@ class CommentListEntry extends React.Component {
   }
 
   onDeleteClickHandler() {
-    console.log('click')
+    //find commentId
+    //if the comment id in response matches the comment id of the post where 'delete was clicked',
+    //take that comment id
+    //and replace it in the delete url
+    axios.get('/api')
+      .then(res => {
+        res.data.forEach((comment) => {
+
+        })
+      })
+    axios.delete(`comment/${commentId}`)
   }
+
+  onEditClickHandler() {
+
+  }
+
   render() {
     console.log('entry props', this.props)
     return (
-      < div >
+      < div className="commentEntry">
         <ReactMarkdown source={this.props.comment.body} />
-        <a onClick={this.onDeleteClickHandler}>delete</a>
+        <div id="deleteComment">
+          <a onClick={this.onDeleteClickHandler}>delete</a>
+        </div>
+        <div id="editComment">
+          <a onClick={this.onEditClickHandler}>edit</a>
+        </div>
         <CommentList comments={this.props.comment.comments} />
+        <style> {`
+          .commentEntry {
+            width: 98%;
+            margin: auto;
+          }
+          #deleteComment a, #editComment a {
+            display: inline-block;
+            font-size: 11px;
+            text-transform: uppercase;
+          }
+          a:hover {
+            color: #A9A9A9;
+          }
+        `}
+        </style>
       </div >
+
+
     )
   }
 }
