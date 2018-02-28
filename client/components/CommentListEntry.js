18a2fefc5a4d8311070e1f1c3343b8f7226138fd
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-27 22:06:56 -0800

    using library for paper style comments

--- client/components/CommentListEntry.js
+++ client/components/CommentListEntry.js
@@ -1,5 +1,7 @@
 import CommentList from './CommentList';
 import ReactMarkdown from 'react-markdown';
+import Paper from 'material-ui/Paper';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class CommentListEntry extends React.Component {
   constructor(props) {
@@ -7,10 +9,17 @@ class CommentListEntry extends React.Component {
     this.state = {
       isReplyBoxHidden: true
     }
+    const style = {
+      height: 100,
+      width: 100,
+      margin: 20,
+      textAlign: 'center',
+      display: 'inline-block',
+    };
   }
 
   onReplyClickHandler() {
-    //show commentForm
+    //when clicked, this.setState isReplyBoxHidden to false
 
   }
 
@@ -19,12 +28,12 @@ class CommentListEntry extends React.Component {
     //if the comment id in response matches the comment id of the post where 'delete was clicked',
     //take that comment id
     //and replace it in the delete url
-    axios.get('/api')
-      .then(res => {
-        res.data.forEach((comment) => {
+    // axios.get('/api')
+    //   .then(res => {
+    //     res.data.forEach((comment) => {
 
-        })
-      })
+    //     })
+    //   })
     axios.delete(`comment/${commentId}`)
   }
 
@@ -38,19 +47,22 @@ class CommentListEntry extends React.Component {
     console.log('entry props', this.props)
     return (
       <div>
-        < div className="commentEntry">
-          <ReactMarkdown source={this.props.comment.body} />
-          <div id="replyComment"></div>
-          <a onClick={this.onReplyClickHandler}>reply</a>
-        </div>
-        <div id="deleteComment">
-          <a onClick={this.onDeleteClickHandler}>delete</a>
-        </div>
-        <div id="editComment">
-          <a onClick={this.onEditClickHandler}>edit</a>
-        </div>
-        <CommentList comments={this.props.comment.comments} />
-        <style> {`
+        <MuiThemeProvider>
+          <Paper style={this.style} zDepth={2} className="commentEntry">
+            <ReactMarkdown source={this.props.comment.body} />
+          </Paper>
+
+          <div id="replyComment">
+            <a onClick={this.onReplyClickHandler}>reply</a>
+          </div>
+          <div id="deleteComment">
+            <a onClick={this.onDeleteClickHandler}>delete</a>
+          </div>
+          <div id="editComment">
+            <a onClick={this.onEditClickHandler}>edit</a>
+          </div>
+          <CommentList comments={this.props.comment.comments} />
+          <style> {`
           .commentEntry {
             width: 98%;
             margin: auto;
@@ -64,8 +76,9 @@ class CommentListEntry extends React.Component {
             color: #A9A9A9;
           }
         `}
-        </style>
-      </div>
+          </style>
+        </MuiThemeProvider >
+      </div >
     )
   }
 }
