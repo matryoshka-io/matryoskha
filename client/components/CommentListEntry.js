506be825789d8088ec224f417c8fd8d70858e277
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-02-27 17:17:33 -0800

    added delete a tag

--- client/components/CommentListEntry.js
+++ client/components/CommentListEntry.js
@@ -5,11 +5,16 @@ class CommentListEntry extends React.Component {
   constructor(props) {
     super(props)
   }
+
+  onDeleteClickHandler() {
+    console.log('click')
+  }
   render() {
     console.log('entry props', this.props)
     return (
       < div >
         <ReactMarkdown source={this.props.comment.body} />
+        <a onClick={this.onDeleteClickHandler}>delete</a>
         <CommentList comments={this.props.comment.comments} />
       </div >
     )
