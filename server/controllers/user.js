91acd50c9d9b891e4118f02b60d506b24dc1d2f9
Paul Valenzano <paul.valenzano@gmail.com>
2018-03-02 15:54:39 -0800

    [subscriptions] send results as json

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -59,7 +59,7 @@ module.exports = {
             .populate('subreddit')
             .exec();
         })
-        .then(subscriptions => res.status(200).send(subscriptions))
+        .then(subscriptions => res.status(200).json(subscriptions))
         .catch(err => res.status(500).send(err));
     },
   },
