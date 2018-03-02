9713832c74a55920e17e0ce45149b2588f16264d
Serghei Madan <madan.serghei@yahoo.com>
2018-03-01 18:24:06 -0800

    added some details for profile page

--- client/pages/user/profile.js
+++ client/pages/user/profile.js
@@ -1,12 +1,35 @@
-import Page from '../../components/Page';
 import { Component } from 'react';
+import Page from '../../components/Page';
+import Posts from '../../components/Posts';
 import Logo from '../../components/Logo'
 
+import auth from '../../utils/auth';
+import profile from '../../utils/profile';
+import data from '../../utils/data';
+import sessions from '../../utils/sessions';
+
 class Profile extends Component {
+  static async getInitialProps(context) {
+    const session = await auth.initializeSession(context);
+    const posts = await data.getPosts(session);
+
+    return {
+      subreddit: context.query.sub || null,
+      user: session.user,
+      token: session.token,
+      posts,
+    };
+  }
+
+
+
   constructor (props) {
     super(props)
     this.state = {
-
+      subreddit: this.props.subreddit,
+      user: this.props.user,
+      token: this.props.token,
+      posts: this.props.posts,
     }
   }
 
@@ -14,15 +37,16 @@ class Profile extends Component {
 
 
   render () {
+    console.log(this.props.subreddit)
     return(
       <Page>
         <div className="posts" >
-          <h3>Your Posts</h3>
+          {this.state.posts}
         </div>
         <div className="accountInfo" >
           <div>
             <Logo />
-            <p>Username: </p>
+            <h2>{this.props.user.username}</h2>
             <button className="newPost">Create New Post</button>
           </div>
           <p>Your Info</p>
@@ -30,10 +54,10 @@ class Profile extends Component {
             
             <div className="birthday">
               <img src="https://vignette.wikia.nocookie.net/kpop/images/2/21/Birthday_Icon.png/revision/latest?cb=20161105130102" width={40} height={40}/>
-              <div>
+              <span>
                 <div>mtryska birthday</div>
                 <div>February 20, 2018</div>
-              </div>
+              </span>
             </div>
             <div className="Trophy">
               <img src="https://upload.wikimedia.org/wikipedia/commons/c/cb/Circle-icons-trophy.svg" width={40} height={40}/>
@@ -63,13 +87,13 @@ class Profile extends Component {
          .posts {
           border: solid 2px;
           float: left;
-          width: 70%;
+          width: 78%;
           height: 600px;
         }
         .accountInfo {
           border: solid 2px;
           float: right;
-          width: 25%;
+          width: 20%;
           height: 600px;
         } * {
           border:1
