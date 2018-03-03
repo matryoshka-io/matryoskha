ced03fbb1644b4ed399b62ebfc9afe339575e66d
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-03 12:23:50 -0800

    [front-end] change to functional

--- client/components/CommentList.js
+++ client/components/CommentList.js
@@ -4,54 +4,38 @@ import ReactMarkdown from 'react-markdown';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Paper from 'material-ui/Paper';
 
-class CommentList extends React.Component {
-  constructor(props) {
-    super(props);
-    const style = {
-      height: 100,
-      width: 100,
-      margin: 20,
-      textAlign: 'center',
-      display: 'inline-block',
-    };
-  }
+const CommentList = (props) => {
+  return (
+    <div>
+      <div className="list">
+        {(props.comments && props.comments.map) &&
+          props.comments.map((comment, index) => {
+            return (
+              <CommentListEntry
+                key={comment._id}
+                index={index}
+                comment={comment} x
+                comments={props.comments}
+                allComments={props.allComments}
+                newCommentBody={props.newCommentBody}
+                postId={props.postId}
+                updateCommentList={props.updateCommentList}
+              />
+            )
+          })}
+      </div>
 
-  componentWillReceiveProps(newProps) {
-    this.setState({ comments: newProps })
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="list">
-          {(this.props.comments && this.props.comments.map) &&
-            this.props.comments.map((comment, index) => {
-              return (
-                <CommentListEntry
-                  key={comment._id}
-                  index={index}
-                  comment={comment}
-                  comments={this.props.comments}
-                  allComments={this.props.allComments}
-                  newCommentBody={this.props.newCommentBody}
-                  postId={this.props.postId}
-                  updateCommentList={this.props.updateCommentList}
-                />
-              )
-            })}
-        </div>
-
-        <style>{`
+      <style>{`
           .list {
             // border-style: solid;
             border-width: 1px 1px 0px 1px;
             border-color: gray;
           }
           `}
-        </style>
-      </div >
-    )
-  }
+      </style>
+    </div >
+  )
 }
+
 
 export default CommentList;
