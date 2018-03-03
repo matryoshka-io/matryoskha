52bc182231722da0780df578f66c7d70f8d8d7ff
Paul Valenzano <paul.valenzano@gmail.com>
2018-03-03 13:12:55 -0800

    [profile] subnav stub

--- client/pages/user/profile.js
+++ client/pages/user/profile.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import moment from 'moment';
+import Link from 'next/link';
 
 import Page from '../../components/Page';
 import Posts from '../../components/Posts';
@@ -33,51 +34,93 @@ class Profile extends Component {
     const { user, profile, content } = this.state;
     return (
       <Page>
-        <div className="posts" >
-          <Posts
-            posts={content}
-            vote={this.castVote}
-          />
-        </div>
-        <div className="accountInfo" >
-          <div>
-            <h2>{user}</h2>
+        <div className="container">
+          <div className="profile__content">
+            <div>
+              <ul className="profile__navigation">
+                <li><Link href={`/u/${user}/`}><a>Posts</a></Link></li>
+                <li><Link href={`/u/${user}/comments`}><a>Comments</a></Link></li>
+                <li><Link href={`/u/${user}/subreddits`}><a>Subreddits</a></Link></li>
+              </ul>
+            </div>
+            <Posts
+              posts={content}
+              vote={this.castVote}
+            />
+          </div>
+          <div className="profile__sidebar">
+            <h3>{user}</h3>
             <span>Total Karma: {profile.karma}</span>
             <span>Joined: {moment(profile.date).fromNow()} </span>
             <button className="newPost">Create New Post</button>
           </div>
         </div>
-
         <style jsx>
           {`
-          .newPost  {
-            align: center;
-          }
-          .Trophy {
-            display:inline-flex;
-            padding-top: 30px;
-          }
+            .container {
+              display: flex;
+              flex-direction: row;
+            }
+            .profile__navigation {
+              margin: 0;
+              height: 40px;
+              width: 100%;
+              display: flex;
+              flex-direction: row;
+              align-items: center;
+              justify-content: stretch;
+            }
+            .profile__navigation > * {
+              height: 40px;
+              text-align: center;
+              list-style: none;
+              flex: 1;
+            }
+            .profile__navigation a {
+              cursor: pointer;
+              font-size: 16px;
+              font-weight: 700;
+            }
+            .profile__navigation a:hover {
+            }
+            .profile__content {
+              border: solid 1px #333;
+              margin-right: 8px;
+              flex: 9;
+            }
+            .profile__sidebar {
+              border: solid 1px #333;
+              padding: 8px;
+              flex: 1 200px;
+            }
+            .newPost  {
+              align: center;
+            }
+            .Trophy {
+              display:inline-flex;
+              padding-top: 30px;
+            }
 
-          .birthday {
-            display:inline-flex;
-          }
-          h3 {
-            text-align: center;
-          }
-          .posts {
-            border: solid 2px;
-            float: left;
-            width: 78%;
-            height: 600px;
-          }
-          .accountInfo {
-            border: solid 2px;
-            float: right;
-            width: 20%;
-            height: 600px;
-          } * {
-            border:1
-          }
+            .birthday {
+              display:inline-flex;
+            }
+            h3 {
+              text-align: center;
+            }
+            .posts {
+              border: solid 2px;
+              float: left;
+              width: 78%;
+              height: 600px;
+            }
+            .accountInfo {
+              border: solid 2px;
+              float: right;
+              width: 20%;
+              height: 600px;
+            } * {
+              border:1
+            }
           `}
         </style>
       </Page>
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -16,20 +16,21 @@ module.exports = {
   },
   posts: {
     GET(req, res) {
-      models.User.findOne({ username: req.params.username }).then((user) => {
-        models.Post.find({ author: user._id, type: { $not: /Comment/ } })
+      models.User.findOne({ username: req.params.username })
+        .then(user => models.Post.find({ author: user._id, type: { $not: /Comment/ } })
           .populate('subreddit')
           .populate('author')
-          .then((posts) => {
-            const promises = [];
-            posts.forEach((post) => {
-              promises.push(utils.matryoksha(req, post));
-            });
-            Promise.all(promises).then(() => {
-              res.status(200).json(posts);
-            });
+          .exec())
+        .then((posts) => {
+          const promises = [];
+          posts.forEach((post) => {
+            promises.push(utils.matryoksha(req, post));
           });
-      });
+          Promise.all(promises).then(() => {
+            res.status(200).json(posts);
+          });
+        })
+        .catch(err => res.status(500).send(err));
     },
   },
   comments: {
