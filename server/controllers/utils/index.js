d6648775e1d504f27fb31a4b8b9bcbc563c2bde7
James Yen <jameshyen@gmail.com>
2018-03-02 16:43:19 -0800

    Complete merge

--- server/controllers/utils/index.js
+++ server/controllers/utils/index.js
@@ -57,17 +57,6 @@ const matryoksha = (req, post, depth = 0) =>
     if (depth > 2) {
       resolve();
     } else {
-<<<<<<< Updated upstream
-      models.Post.find({ type: 'Comment', parent: post._id }).lean().then((comments) => {
-        getKarmaAndSort(comments, (comments) => {
-          post.comments = comments;
-          const promises = [];
-          post.comments.forEach((comment) => {
-            promises.push(matryoksha(comment, depth + 1));
-          });
-          Promise.all(promises).then(() => {
-            resolve();
-=======
       models.Post.find({ type: 'Comment', parent: post._id })
         .populate('author')
         .lean()
@@ -81,10 +70,9 @@ const matryoksha = (req, post, depth = 0) =>
             Promise.all(promises).then(() => {
               resolve();
             });
->>>>>>> Stashed changes
+
           });
         });
-      });
     }
   });
 
