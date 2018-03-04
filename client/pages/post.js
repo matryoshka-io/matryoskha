94b0619f356cf8b8052972877296eb19c893909f
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-04 14:32:59 -0800

    [front end] accepting query

--- client/pages/post.js
+++ client/pages/post.js
@@ -12,28 +12,19 @@ import vote from '../utils/votes';
 import sessions from '../utils/sessions';
 import ParentPost from '../components/ParentPost'
 
+import 'isomorphic-fetch';
+
 class PostDetailPage extends Component {
-  // static async getInitialProps(context) {
-  //   const initialProps = await data.prepPostDetailView(context);
-  //   return initialProps;
   static async getInitialProps({ query }) {
     const post = await fetch(`http://localhost:3000/api/post/${query.post}`);
     const json = await post.json();
     return json;
   }
-  // }
+
 
   constructor(props) {
     super(props);
-    // this.state = {
-    //   title: this.props.title,
-    //   subreddit: this.props.subreddit,
-    //   user: this.props.user,
-    //   post: {},
-    //   comments: [],
-    // }
     this.state = props;
-    console.log('this.state from post page', this.state)
   }
 
   render() {
@@ -43,7 +34,6 @@ class PostDetailPage extends Component {
         <div className="pageContent">
           <div className="posts" >
             <ParentPost {...this.state} />
-            There be dragons here
           </div>
           <div className="sidebar" >
             <UserPanelBody
