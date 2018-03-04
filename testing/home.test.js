ca2b612a8b5219321546a5256d669462ba6ae952
James Yen <jameshyen@gmail.com>
2018-03-03 16:51:58 -0800

    Seed data tests may be invalidated if anyone changes data.json

--- testing/home.test.js
+++ testing/home.test.js
@@ -1,10 +1,28 @@
 const request = require('superagent');
 
-test('get homepage as "admin"', (done) => {
+test('seed data: "admin"\'s homepage should be populated (despite having no subscriptions)', (done) => {
   request
     .get('http://localhost:3000/api/')
     .end((err, res) => {
       expect(res.body.length).toBeGreaterThan(0);
+      done();
+    });
+});
+
+test('seed data: "admin"\'s homepage should contain 6 posts', (done) => {
+  request
+    .get('http://localhost:3000/api/')
+    .end((err, res) => {
+      expect(res.body.length).toBe(6);
+      done();
+    });
+});
+
+test('seed data: "admin" should have voted on the top post', (done) => {
+  request
+    .get('http://localhost:3000/api/')
+    .end((err, res) => {
+      expect(res.body[0].voted).toBe(1);
       done();
     });
 });
