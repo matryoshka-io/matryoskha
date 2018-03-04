8d50e8d37d1c960cb2e228ee62e607bb2e44cf4e
James Yen <jameshyen@gmail.com>
2018-03-03 18:06:00 -0800

    More tests

--- testing/useractions.test.js
+++ testing/useractions.test.js
@@ -1,5 +1,10 @@
 const request = require('superagent');
 
+const ken = '5a8e0e077f911450d4600d96' // ken's id, as per seed data
+
+let post;
+let comment;
+
 test('make a subreddit', (done) => {
   request
     .post('http://localhost:3000/api/sub')
@@ -9,7 +14,7 @@ test('make a subreddit', (done) => {
     })
     .end((err, res) => {
       expect(res.body).toBeTruthy();
-      expect(res.body.creator).toBe('5a8e0e077f911450d4600d96'); // this is ken's id
+      expect(res.body.creator).toBe(ken);
       expect(res.body.titleSlug).toBe('nextjs');
       done();
     });
@@ -24,9 +29,40 @@ test('post in a subreddit', (done) => {
       description: 'First post!',
     })
     .end((err, res) => {
+      post = res.body._id;
       expect(res.body).toBeTruthy();
-      expect(res.body.author).toBe('5a8e0e077f911450d4600d96');
+      expect(res.body.author).toBe(ken);
       expect(res.body.titleSlug).toBe('nextjs');
+      done();
+    });
+});
+
+test('comment on a post', (done) => {
+  request
+    .post(`http://localhost:3000/api/post/${post}`)
+    .send({
+      body: 'boooooo',
+    })
+    .end((err, res) => {
+      comment = res.body._id;
+      expect(res.body).toBeTruthy();
+      expect(res.body.author).toBe(ken);
+      expect(res.body.body).toBe('boooooo'); // DRY...
+      done();
+    });
+});
+
+test('comment on a comment', (done) => {
+  request
+    .post(`http://localhost:3000/api/comment/${comment}`)
+    .send({
+      body: 'i\'m back!',
+    })
+    .end((err, res) => {
+      comment = res.body._id;
+      expect(res.body).toBeTruthy();
+      expect(res.body.author).toBe(ken);
+      expect(res.body.body).toBe('i\'m back!'); // DRY, DRY...
       done();
     });
 });
