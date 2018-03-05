f4599daf7689e9ae3172b01813aeabb396ad365c
James Yen <jameshyen@gmail.com>
2018-03-05 13:10:19 -0800

    Submit links

--- client/components/LinkBar.js
+++ client/components/LinkBar.js
@@ -1,8 +1,16 @@
-const LinkBar = props => (
-  <div>
-    Video and Img Link: <br />
-    <input val="text" /> <br />
-  </div>
-);
+class LinkBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Link: <br />
+        <input value={this.link} onChange={this.props.linkChange} /> <br />
+      </div>      
+    );
+  }
+}
 
 export default LinkBar;
