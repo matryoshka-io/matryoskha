e2285e9361fb2987f7621141f91a4161253a288c
Meeyoung Kim <alicemeeyoungkim@gmail.com>
2018-03-05 14:55:59 -0800

    [ui] drop down looks more consistent

--- client/components/SubscribedSubreddits.js
+++ client/components/SubscribedSubreddits.js
@@ -12,40 +12,41 @@ const SubscribedSubreddits = ({ subscriptions }) => {
       </div>
       <style>{`
     .dropdown-button {
-      background-color: #696775;
-      color: white;
-      padding: 10px;
-      font-size: 10px;
+      background-color: transparent;
+      color: #696775;
+      font-size: 12px;
+      font-family: 'PT Mono';
+      font-weight: bold;
+      text-transform: uppercase;
       border: none;
-  }
-  .dropdown {
-    position: relative;
-    display: inline-block;
-  }
-  .dropdown-links {
-    display: none;
-    position: absolute;
-    background-color: #f1f1f1;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-  }
-  .dropdown-links a {
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-  }
-  .dropdown-links a:hover {background-color: #696775}
+    }
+    .dropdown {
+      position: relative;
+      display: inline-block;
+    }
+    .dropdown-links {
+      display: none;
+      position: absolute;
+      background-color: #f1f1f1;
+      min-width: 160px;
+      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+      z-index: 1;
+    }
+    .dropdown-links a {
+      color: black;
+      padding: 12px 16px;
+      text-decoration: none;
+      display: block;
+    }
+    .dropdown-links a:hover {background-color: #696775}
 
-  .dropdown:hover .dropdown-links {
-    display: block;
-  }
+    .dropdown:hover .dropdown-links {
+      display: block;
+    }
 
-  .dropdown:hover .dropdown-button {
-    background-color: #696775;
-    font-color: white;
-  }
+    .dropdown:hover .dropdown-button {
+      font-color: white;
+    }
     `}</style>
     </div>
   )
